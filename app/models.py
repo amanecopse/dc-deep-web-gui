a46d9a535633bc7ff7448125bb070d541838dcbd
amanecopse <amanecopse@gmail.com>
2022-08-02 12:34:38 +0900

    Add django model to record PDU data

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime, timedelta
 from django.db import models
 
-# Create your models here.
+
+def resetModel(model: models.Model):
+    model.objects.all().delete()
+
+
+def getDataFromNDaysAgo(model: models.Model, nDays):
+    model.objects.filter(dateTime__gte=datetime.today() -
+                         timedelta(days=nDays))
+
+
+class StoredPduData(models.Model):
+    dateTime = models.DateTimeField(auto_now_add=True)
+    outputNum = models.IntegerField()
+    current = models.DecimalField(max_digits=20, decimal_places=2)
+    power = models.DecimalField(max_digits=20, decimal_places=2)
+    energyCounter = models.DecimalField(max_digits=20, decimal_places=2)
+    tpf = models.DecimalField(max_digits=20, decimal_places=2)
+    phaseShift = models.DecimalField(max_digits=20, decimal_places=2)
+    reverseEnergyCounter = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return f'output: {self.outputNum}, date:{self.dateTime}'
