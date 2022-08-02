d2b4701d7de59901a1133247aea8301f4e7ab624
amanecopse <amanecopse@gmail.com>
2022-08-02 15:09:42 +0900

    Make AppConfig to be overriden

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+# from background_task.models import Task
+# from app.tasks import storePduData
+
+# class AppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'app'
 
 
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class MyAppConfig(AppConfig):
     name = 'app'
+    verbose_name = 'My App Config'
+
+    # def ready(self):
+    #     from background_task.models import Task
+    #     from app.tasks import storePduData
+
+    #     tasks = Task.objects.filter(verbose_name="store_pdu")
+    #     if len(tasks) == 0:
+    #         storePduData(repeat=20, verbose_name="store_pdu")
+    #     else:
+    #         return
