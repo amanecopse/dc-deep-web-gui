fc72a026caf94fe9e29c5f2118ea4167ce1e68cf
amanecopse <amanecopse@gmail.com>
2022-08-09 16:22:43 +0900

    Add models in env app

--- project/apps/env/models.py
+++ project/apps/env/models.py
@@ -1,15 +1,65 @@
 from django.db import models
 
+# message type values: Temperature Humidity Co2 Light Camera Motor
+# keys: type celsius rh ppm lux index status
 
-class Containment(models.Model):
+
+class Temperature(models.Model):
+
+    mac = models.CharField(max_length=20)
     dateTime = models.DateTimeField(auto_now_add=True)
-    outputNum = models.IntegerField()
-    current = models.DecimalField(max_digits=20, decimal_places=2)
-    power = models.DecimalField(max_digits=20, decimal_places=2)
-    energyCounter = models.DecimalField(max_digits=20, decimal_places=2)
-    tpf = models.DecimalField(max_digits=20, decimal_places=2)
-    phaseShift = models.DecimalField(max_digits=20, decimal_places=2)
-    reverseEnergyCounter = models.DecimalField(max_digits=20, decimal_places=2)
+    celsius = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f'output: {self.outputNum}, date:{self.dateTime}'
+        return f'mac: {self.mac}, date:{self.dateTime}, celsius: {self.celsius}'
+
+
+class Humidity(models.Model):
+
+    mac = models.CharField(max_length=20)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    rh = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'mac: {self.mac}, date:{self.dateTime}, rh: {self.rh}'
+
+
+class Co2(models.Model):
+
+    mac = models.CharField(max_length=20)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    ppm = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'mac: {self.mac}, date:{self.dateTime}, ppm: {self.ppm}'
+
+
+class Light(models.Model):
+
+    mac = models.CharField(max_length=20)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    lux = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'mac: {self.mac}, date:{self.dateTime}, lux: {self.lux}'
+
+
+class Camera(models.Model):
+
+    mac = models.CharField(max_length=20)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    index = models.CharField(max_length=20)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return f'mac: {self.mac}, date:{self.dateTime}, index: {self.index}, status: {self.status}'
+
+
+class Motor(models.Model):
+
+    mac = models.CharField(max_length=20)
+    dateTime = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return f'mac: {self.mac}, date:{self.dateTime}, status: {self.status}'
